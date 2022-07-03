edf0f6227fe0797338566e523c7f8294704ffec8
natruz <88256962+natruz@users.noreply.github.com>
2022-07-03 20:48:49 +0800

    Initial Commit

---
+++ studentCongress-stampCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  studentCongress-stampCard
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 3/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ studentCongress-stampCard/studentCongress_stampCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  studentCongress_stampCardApp.swift
+//  studentCongress-stampCard
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct studentCongress_stampCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
